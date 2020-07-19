b5f34ac70abe9e84550955e7bf6023b1eb2b0a3e
Cris Jr <crisjr@pm.me>
2020-07-19 16:09:25 -0300

    Structure callbacks for every institutions currently desired

--- maps/society.go
+++ maps/society.go
@@ -13,6 +13,7 @@ type Institution struct {
     Where []int
     // javascript object holding institution's memory
     Memory string
+    // TODO define memory format for every institution
 }
 
 // Updates the society `s` living on the world `m`
@@ -20,6 +21,15 @@ func Tick(world [][]Block, institutions []Institution) []Institution {
     functions := map[string]func([][]Block, []Institution, int)[]Institution {
         "farm": updateFarm,
         "house": updateHouse,
+        "bank": updateBank,
+        "factory": updateFactory,
+        "port": updatePort,
+        "atelier": updateAtelier,
+        "path": updatePath,
+        "mine": updateMine,
+        "shop": updateShop,
+        "guild": updateGuild,
+        "laboratory": updateLaboratory,
     }
 
     for i := 0; i < len(institutions); i++ {
@@ -43,6 +53,42 @@ func updateFarm(world [][]Block, society[]Institution, index int) []Institution 
     return society
 }
 
-func updateHouse(world [][]Block, society[]Institution, house int) []Institution {
+func updateHouse(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateBank(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateFactory(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updatePort(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateAtelier(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updatePath(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateMine(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateShop(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateGuild(world [][]Block, society[]Institution, index int) []Institution {
+    return society
+}
+
+func updateLaboratory(world [][]Block, society[]Institution, index int) []Institution {
     return society
 }
